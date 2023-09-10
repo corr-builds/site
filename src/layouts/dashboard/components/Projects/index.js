b43adb8a139b748003629fcf07b66415b1317058
Cathleen Corr <c.corr.dev@gmail.com>
2023-09-10 18:32:12 -0500

    simplifying dashboard

--- src/layouts/dashboard/components/Projects/index.js
+++ src/layouts/dashboard/components/Projects/index.js
@@ -59,49 +59,7 @@ function Projects() {
     </Menu>
   );
 
-  return (
-    <Card>
-      <SoftBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
-        <SoftBox>
-          <SoftTypography variant="h6" gutterBottom>
-            Projects
-          </SoftTypography>
-          <SoftBox display="flex" alignItems="center" lineHeight={0}>
-            <Icon
-              sx={{
-                fontWeight: "bold",
-                color: ({ palette: { info } }) => info.main,
-                mt: -0.5,
-              }}
-            >
-              done
-            </Icon>
-            <SoftTypography variant="button" fontWeight="regular" color="text">
-              &nbsp;<strong>30 done</strong> this month
-            </SoftTypography>
-          </SoftBox>
-        </SoftBox>
-        <SoftBox color="text" px={2}>
-          <Icon sx={{ cursor: "pointer", fontWeight: "bold" }} fontSize="small" onClick={openMenu}>
-            more_vert
-          </Icon>
-        </SoftBox>
-        {renderMenu}
-      </SoftBox>
-      <SoftBox
-        sx={{
-          "& .MuiTableRow-root:not(:last-child)": {
-            "& td": {
-              borderBottom: ({ borders: { borderWidth, borderColor } }) =>
-                `${borderWidth[1]} solid ${borderColor}`,
-            },
-          },
-        }}
-      >
-        <Table columns={columns} rows={rows} />
-      </SoftBox>
-    </Card>
-  );
+  return;
 }
 
 export default Projects;
