5241b6a06513d40ea53428b702e93b28eeb1bc1c
Cathleen Corr <c.corr.dev@gmail.com>
2023-09-13 22:32:34 -0500

    start adding data

--- src/layouts/dashboard-tech/index.js
+++ src/layouts/dashboard-tech/index.js
@@ -54,33 +54,33 @@ function DashboardTech() {
           <Grid container spacing={3}>
             <Grid item xs={12} sm={6} xl={3}>
               <MiniStatisticsCard
-                title={{ text: "today's money" }}
+                title={{ text: "Years Experience" }}
                 count="$53,000"
-                percentage={{ color: "success", text: "+55%" }}
+                // percentage={{ color: "success", text: "+55%" }}
                 icon={{ color: "info", component: "paid" }}
               />
             </Grid>
             <Grid item xs={12} sm={6} xl={3}>
               <MiniStatisticsCard
-                title={{ text: "today's users" }}
+                title={{ text: "Companies" }}
                 count="2,300"
-                percentage={{ color: "success", text: "+3%" }}
+                // percentage={{ color: "success", text: "+3%" }}
                 icon={{ color: "info", component: "public" }}
               />
             </Grid>
             <Grid item xs={12} sm={6} xl={3}>
               <MiniStatisticsCard
-                title={{ text: "new clients" }}
+                title={{ text: "Books & Courses" }}
                 count="+3,462"
-                percentage={{ color: "error", text: "-2%" }}
+                // percentage={{ color: "error", text: "-2%" }}
                 icon={{ color: "info", component: "emoji_events" }}
               />
             </Grid>
             <Grid item xs={12} sm={6} xl={3}>
               <MiniStatisticsCard
-                title={{ text: "sales" }}
+                title={{ text: "Technologies" }}
                 count="$103,430"
-                percentage={{ color: "success", text: "+5%" }}
+                // percentage={{ color: "success", text: "+5%" }}
                 icon={{
                   color: "info",
                   component: "shopping_cart",
